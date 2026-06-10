d5809c0a4a6d213a9b1559ba89f37486b83ae413
Chris J. Miller <godservantschris@gmail.com>
2026-06-10 18:12:39 -0400

    SetEnvVars.py - Update endpoiint and model  vars

--- InternalModules/OpenAiEnvironment/SetEnvVars.py
+++ InternalModules/OpenAiEnvironment/SetEnvVars.py
@@ -1,12 +1,12 @@
 import os
 
 os.environ['OPENAI_TYPE'] = 'azure'
-os.environ['AZURE_SUBSCRIPTION_ID'] = '511700a2-be8e-48ce-84c8-d1c480d9bf53'
+os.environ['AZURE_SUBSCRIPTION_ID'] = 'ff423e87-a552-45d3-be3a-a3a662b074c9'
 # USEAST
-os.environ['AZURE_OPENAI_ENDPOINT'] = 'https://abundantstreamssvcs-openai.openai.azure.com/'
-os.environ['AZURE_OPENAI_API_VERSION'] = '2024-05-01-preview'
-os.environ['AZURE_OPENAI_TEXT_DEPLOYMENT'] = 'gpt-4' 
-os.environ['AZURE_OPENAI_TEXT_MODEL'] = 'gpt-4'
+os.environ['AZURE_OPENAI_ENDPOINT'] = 'https://resumebuilder-openairesource.openai.azure.com/'
+os.environ['AZURE_OPENAI_API_VERSION'] = '2025-08-07'
+os.environ['AZURE_OPENAI_TEXT_DEPLOYMENT'] = 'gpt-4,1' 
+os.environ['AZURE_OPENAI_TEXT_MODEL'] = 'gpt-4.1'
 os.environ['AZURE_OPENAI_TEXT_TOKEN_RATE_LIMIT'] = '1000'
 os.environ['AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT'] = 'text-embedding-ada-002'
 os.environ['AZURE_OPENAI_EMBEDDINGS_MODEL'] = 'text-embedding-ada-002'
