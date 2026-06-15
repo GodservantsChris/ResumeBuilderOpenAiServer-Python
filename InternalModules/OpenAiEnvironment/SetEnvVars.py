ba0d868b8fe087d0705151d28a2f8c0f6520f525
Chris J. Miller <godservantschris@gmail.com>
2026-06-15 10:44:58 -0400

    SetEnvVars.py - os.environ['AZURE_OPENAI_API_VERSION'] = '2024-12-01-preview'

--- InternalModules/OpenAiEnvironment/SetEnvVars.py
+++ InternalModules/OpenAiEnvironment/SetEnvVars.py
@@ -4,7 +4,7 @@ os.environ['OPENAI_TYPE'] = 'azure'
 os.environ['AZURE_SUBSCRIPTION_ID'] = 'ff423e87-a552-45d3-be3a-a3a662b074c9'
 # USEAST
 os.environ['AZURE_OPENAI_ENDPOINT'] = 'https://resumebuilder-openairesource.openai.azure.com/'
-os.environ['AZURE_OPENAI_API_VERSION'] = '2025-08-07'
+os.environ['AZURE_OPENAI_API_VERSION'] = '2024-12-01-preview'
 os.environ['AZURE_OPENAI_TEXT_DEPLOYMENT'] = 'gpt-4.1' 
 os.environ['AZURE_OPENAI_TEXT_MODEL'] = 'gpt-4.1'
 os.environ['AZURE_OPENAI_TEXT_TOKEN_RATE_LIMIT'] = '1000'
